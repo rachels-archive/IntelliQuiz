682e27d21c94c2a04de20e3b5156a826e4b063d5
rachels-archive <rachelhui@protonmail.com>
2024-10-25 01:15:40 +0800

    Add quiz section for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,22 +13,29 @@ const page = async () => {
   }
 
   return (
-    <div className="w-full mt-14 p-4">
+    <div className="w-full mt-14 p-8">
       <h2 className="text-2xl font-bold">{session?.user.username}&rsquo;s Dashboard</h2>
 
-      <div className="grid grid-cols-3 gap-4 mt-4">
-        <div className="bg-white rounded-lg shadow-md py-12 text-center">
-          <h3 className="font-semibold">x</h3>
-          <p>Quizzes Completed</p>
+      <div className="grid grid-cols-3 gap-4 mt-4 bg-[#57463E] p-4">
+        <div className="bg-[#FDE8D3] rounded-lg shadow-md py-16 text-center">
+          <h3 className="font-semibold text-4xl">X</h3>
+          <p className="text-2xl">Quizzes Completed</p>
         </div>
-        <div className="bg-white rounded-lg shadow-md py-12 text-center">
-          <h3 className="font-semibold">x</h3>
-          <p>Average Score</p>
+        <div className="bg-[#FDE8D3] rounded-lg shadow-md py-16 text-center">
+          <h3 className="font-semibold text-4xl">X</h3>
+          <p className="text-2xl">Average Score</p>
         </div>
-        <div className="bg-white rounded-lg shadow-md py-12 text-center">
-          <h3 className="font-semibold">x</h3>
-          <p>Badges Earned</p>
+        <div className="bg-[#FDE8D3] rounded-lg shadow-md py-16 text-center">
+          <h3 className="font-semibold text-4xl">X</h3>
+          <p className="text-2xl">Badges Earned</p>
         </div>
+      </div>
+
+      <h2 className="text-2xl font-bold mt-4">Quiz</h2>
+
+      <div className="w-1/2 grid grid-cols-2 gap-4 mt-4 bg-[#57463E] p-4">
+        <button className="bg-[#FDE8D3] rounded-lg shadow-md py-4 font-semibold text-center">Generate Quiz</button>
+        <button className="bg-[#FDE8D3] rounded-lg shadow-md py-4 font-semibold text-center">Generate Quiz</button>
       </div>
     </div>
   );
