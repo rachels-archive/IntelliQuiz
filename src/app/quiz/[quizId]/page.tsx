85dc981604a5ec8c85150f140d4cff91a280d093
rachels-archive <rachelhui@protonmail.com>
2024-10-26 10:14:13 +0800

    Display questions according to quizId

--- src/app/quiz/[quizId]/page.tsx
+++ src/app/quiz/[quizId]/page.tsx
@@ -1,11 +1,36 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
+import prisma from "@/lib/db";
 
 export const metadata = {
   title: "Playing Quiz...",
 };
 
-const page = () => {
-  return <div>page</div>;
+type Props = {
+  params: {
+    quizId: string;
+  };
+};
+
+const page = async ({ params: { quizId } }: Props) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  const quiz = await prisma.quiz.findUnique({
+    where: {
+      id: quizId,
+    },
+    include: {
+      questions: true,
+    },
+  });
+
+  return <pre>{JSON.stringify(quiz, null, 2)}</pre>;
 };
 
 export default page;
